8e604ead950112f7e81a73ebcb45c5c08f0764e0
Olivier FAURAX <olivier+git@faurax.fr>
2024-04-29 14:09:20 +0200

    theme: hmap: cleanup old markup

--- themes/hmap/theme.php
+++ themes/hmap/theme.php
@@ -4,9 +4,9 @@ class ThemeHMAP implements Itheme
 {
     public function getCSS()
     {
-echo '<link href="//www.hmap.fr/wp-content/themes/twentyeleven-hmap/style.css" media="all" type="text/css" rel="stylesheet"></link>';
-echo "
-<style type=\"text/css\">
+        echo '<link href="//www.hmap.fr/wp-content/themes/twentyeleven-hmap/style.css" media="all" type="text/css" rel="stylesheet">';
+        echo "
+<style>
 @font-face {
     font-family: 'PoppeaRegular';
     src: url('themes/hmap/poppea-webfont.woff') format('woff');
@@ -15,8 +15,8 @@ echo "
 }
 </style>
 ";
-echo '
-<style type="text/css">
+        echo '
+<style>
 h1, h2, h3
 {
 	font-family: PoppeaRegular, serif;
@@ -28,7 +28,7 @@ h1, h2, h3
 
     public function getBanner()
     {
-        echo '<a href="//hmap.fr/arvin2/"><img width="" height="100" alt="" style="" src="//www.hmap.fr/wp-content/uploads/2017/12/logo-droite-2lignes-1024x257.png" /></a>';
+        echo '<a href="//hmap.fr/arvin2/"><img height="100" alt="" style="" src="//www.hmap.fr/wp-content/uploads/2017/12/logo-droite-2lignes-1024x257.png"></a>';
     }
 }
 
